d24dc81b389f1dca315043080a7904e970023c6f
Damon <damonkennes@gmail.com>
2025-11-03 08:57:50 +0100

    initial datamodel

---
+++ config/dispatcher/dispatcher.ex
@@ -0,0 +1,43 @@
+defmodule Dispatcher do
+  use Matcher
+  define_accept_types [
+    html: [ "text/html", "application/xhtml+html" ],
+    json: [ "application/json", "application/vnd.api+json" ]
+  ]
+
+  @any %{}
+  @json %{ accept: %{ json: true } }
+  @html %{ accept: %{ html: true } }
+
+  define_layers [ :static, :services, :fall_back, :not_found ]
+
+  # In order to forward the 'themes' resource to the
+  # resource service, use the following forward rule:
+  #
+  # match "/themes/*path", @json do
+  #   Proxy.forward conn, path, "http://resource/themes/"
+  # end
+  #
+  # Run `docker-compose restart dispatcher` after updating
+  # this file.
+
+  match "/albums/*path", @json do
+    Proxy.forward conn, path, "http://resource/albums/"
+  end
+
+  match "/artists/*path", @json do
+    Proxy.forward conn, path, "http://resource/artists/"
+  end
+
+  match "/ratings/*path", @json do
+    Proxy.forward conn, path, "http://resource/ratings/"
+  end
+
+  match "/users/*path", @json do
+    Proxy.forward conn, path, "http://resource/users/"
+  end
+
+  match "/*_", %{ layer: :not_found } do
+    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  end
+end
